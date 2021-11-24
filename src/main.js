5fc380a96cf87763918f5ef818197cf4adb71f5a
Simon <Simon.Kaemmer@yahoo.de>
2021-11-24 20:58:26 +0100

    Added axios

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import axios from "axios";
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
   router,
-  render: h => h(App)
+  render: h => h(App),
+  mounted() {
+    axios.get().then()
+  }
 }).$mount('#app')
