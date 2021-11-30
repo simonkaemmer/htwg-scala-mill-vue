b01aab774ebf4c53de0d68279014418187870ca2
Kevin Schoch <kevin.schoch@outlook.com>
2021-11-30 16:12:44 +0100

    Add FooterBar.vue

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,18 @@
     <v-main>
       <router-view/>
     </v-main>
+    <FooterBar />
   </v-app>
 </template>
 
 <script>
 import NavigationBar from "./components/NavigationBar";
+import FooterBar from "./components/FooterBar";
+
 export default {
   name: 'App',
   components: {
+    FooterBar,
     NavigationBar
   }
 };
