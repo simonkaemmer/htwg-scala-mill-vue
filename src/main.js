816b0f4dbf653944ea8191fcf1753aea7f2cdb0d
Kevin Schoch <kevin.schoch@outlook.com>
2021-12-07 12:45:47 +0100

    Add pwa

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
 import VueConfetti from 'vue-confetti'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
